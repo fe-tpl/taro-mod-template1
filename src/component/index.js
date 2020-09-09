bfb01293e149208b508b097742c4c51855e60654
shangshanruoshui <gxgongxueyuan123@163.com>
2020-09-09 14:30:37 +0800

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -2,6 +2,41 @@ import "./index.less";
 import Nerv, { Component } from 'nervjs'; // 这句话不允许做任何改动
 import Taro from "@tarojs/taro"; // 这句话不允许做任何改动
 import { View } from "@tarojs/components";
+
+const baseRate = 10 / 750;
+const convertPxToRem = function (px) {
+  if (Taro.getEnv() == "WEB") {
+    console.log(" =======")
+    return `${px * baseRate}rem`;
+  } else {
+    return `${px}rpx`;
+  }
+};
+
+/**
+ * 将px转换成html
+ * @param {string} tplHtml 模板html
+ * @return {string} 返回转换处理后的html字符串
+ */
+const convertHtmlPxToRem = function (tplHtml) {
+  let regExp = /\d+px/gm;
+  let match = tplHtml.match(regExp);
+  match = match || [];
+  for (let i = 0; i < match.length; i++) {
+    let regExp1 = /(\d+)px/gm;
+    let px = 0;
+    if (regExp1.test(match[i])) {
+      px = Number(RegExp.$1);
+    }
+    if (Taro.getEnv() == "WEB") {
+      tplHtml = tplHtml.replace(`${match[i]}`, `${px * baseRate}rem`);
+    } else {
+      tplHtml = tplHtml.replace(`${match[i]}`, `${px}rpx`);
+    }
+  }
+  return tplHtml;
+}
+
 export default class TaroTpl extends Component {
   constructor(props) {
     super(props)
